d457c1e5b8fd525b9cf2b9b0d947d3ff2c88af97
Ian Snyder <isnide23@gmail.com>
2020-11-04 20:11:56 -0800

    first round

--- bst.py
+++ bst.py
@@ -3,7 +3,47 @@
 # If you can't figure it out recursively, use a loop. (But then refactor
 # your implementation into a recursive one!)
 # Your implementation should pass the tests in test_bst.py.
-# YOUR NAME
+# Ian Snyder
 
 class BinarySearchTree:
-   pass
+
+   def __init__(self, key = None):
+      self.left = None
+      self.right = None
+      self.key = key
+      self.parent = None
+      self.root = self
+
+   def insert(self, child):
+      if child.key <= self.key and self.left == None:
+         self.left = child
+         child.parent = self
+         return
+      elif child.key > self.key and self.right == None:
+         self.right = child
+         child.parent = self
+         return
+      else:
+         if child.key <= self.key:
+            self.left.insert(child)
+            child.parent = self.left
+            return
+         else:
+            self.right.insert(child)
+            child.parent = self.right
+            return
+      
+         
+   def search(self, value):
+      if value == self.key:
+         return self
+      else:
+         return None
+
+   def delete(self, value):
+      if self.key == value:
+         return None
+      else:
+         return self
+   
+
